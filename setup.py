846ddf364d827018fe80f58a98166ac8252abc62
Ishan Oshada <63636533+Ishanoshada@users.noreply.github.com>
2026-08-14 13:07:58 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
-
+   
 setup(
     name="cryptodo",
     version="3.3.1",
